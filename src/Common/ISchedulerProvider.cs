251d51797313fe4b00e60e7e51e7284cad13ba1a
alex6dj <alexeiagueroalba@gmail.com>
2024-04-02 19:48:01 -0400

    Initial Avalonia app build

---
+++ src/Common/ISchedulerProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Reactive.Concurrency;
+
+namespace Common;
+
+public interface ISchedulerProvider
+{
+    IScheduler CurrentThread { get; }
+    IScheduler Dispatcher { get; }
+    IScheduler Immediate { get; }
+    IScheduler NewThread { get; }
+    IScheduler TaskPoolLongRunning { get; }
+    IScheduler TaskPool { get; }
+}
