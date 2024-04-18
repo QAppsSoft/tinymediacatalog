d2952f289f5bfe3536d670279edc4aceb2c1a97e
alex6dj <alexeiagueroalba@gmail.com>
2024-04-18 08:57:13 -0400

    Create Genre to Movie, Many to Many link

--- src/Domain/Models/Genre.cs
+++ src/Domain/Models/Genre.cs
@@ -1,8 +1,11 @@
 ﻿using Domain.Models.BaseObjects;
+using Domain.Models.Movie;
 
 namespace Domain.Models;
 
 public sealed class Genre : GuidEntityBase
 {
     public string Name { get; set; } = string.Empty;
+    
+    public ICollection<MovieContainer> Movies { get; set; } = new List<MovieContainer>();
 }
