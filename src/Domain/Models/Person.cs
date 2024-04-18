3a25e72bfd4e3075b9481f9aae3bd31e1e735da5
alex6dj <alexeiagueroalba@gmail.com>
2024-04-18 08:37:42 -0400

    Remove unused Role in Person Model

--- src/Domain/Models/Person.cs
+++ src/Domain/Models/Person.cs
@@ -5,7 +5,6 @@ namespace Domain.Models;
 public sealed class Person : GuidEntityBase
 {
     public string Name { get; set; } = string.Empty;
-    public string Role { get; set; } = string.Empty;
     public string Thumb { get; set; } = string.Empty;
     public string Profile { get; set; } = string.Empty;
     public ICollection<UniqueId> UniqueIds { get; set; } = new List<UniqueId>();
