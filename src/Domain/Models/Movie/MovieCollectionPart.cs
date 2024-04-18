75cb9cb23ba799e0d4f1d5908ff77ca27ca9a189
alex6dj <alexeiagueroalba@gmail.com>
2024-04-18 08:38:53 -0400

    Simplify MovieCollectionPart Model

--- src/Domain/Models/Movie/MovieCollectionPart.cs
+++ src/Domain/Models/Movie/MovieCollectionPart.cs
@@ -7,12 +7,13 @@ public sealed class MovieCollectionPart : GuidEntityBase
     public string Title { get; set; } = string.Empty;
     public string OriginalTitle { get; set; } = string.Empty;
     public string Overview { get; set; } = string.Empty;
+    public int TmdbId { get; set; }
 
     public string PosterPath { get; set; } = string.Empty;
     public string BackdropPath { get; set; } = string.Empty;
 
-    public ICollection<UniqueId> UniqueIds { get; set; } = new List<UniqueId>();
-    public ICollection<Genre> Genres { get; set; } = new List<Genre>();
+    public string Genres { get; set; } = string.Empty;
+    
     public DateOnly ReleaseDate { get; set; }
 
     public string Note { get; set; } = string.Empty;
