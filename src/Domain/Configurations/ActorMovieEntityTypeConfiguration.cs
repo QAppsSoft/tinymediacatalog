53f73ae37b05ce431c19d707720524f7991e4835
alex6dj <alexeiagueroalba@gmail.com>
2024-04-18 08:15:30 -0400

    Create composite index in ActorMovie model

---
+++ src/Domain/Configurations/ActorMovieEntityTypeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Domain.Configurations;
+
+public sealed class ActorMovieEntityTypeConfiguration : IEntityTypeConfiguration<ActorMovie>
+{
+    public void Configure(EntityTypeBuilder<ActorMovie> builder)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+
+        builder.HasKey(e => new { e.PersonId, e.MovieContainerId });
+    }
+}
--- src/Domain/Models/ActorMovie.cs
+++ src/Domain/Models/ActorMovie.cs
@@ -5,6 +5,8 @@ namespace Domain.Models;
 public sealed class ActorMovie
 {
     public string Role { get; set; } = string.Empty;
+    public Guid PersonId { get; set; }
     public Person Person { get; set; } = null!;
+    public Guid MovieContainerId { get; set; }
     public MovieContainer Movie { get; set; } = null!;
 }
