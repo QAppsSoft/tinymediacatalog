9d407820f7cb0ab1d12d7c5288ca240a0b0a35d3
alex6dj <alexeiagueroalba@gmail.com>
2024-04-17 23:37:18 -0400

    Test database creation and connection string

---
+++ tests/DomainTests/DbContextFactoryTests.cs
@@ -0,0 +1,38 @@
+﻿using Common.Vars;
+using Domain.Factories;
+using Microsoft.EntityFrameworkCore;
+
+namespace DomainTests;
+
+public class DbContextFactoryTests
+{
+    [Test]
+    public void Database_should_be_created()
+    {
+        bool created;
+        var dbcFactory = new DbContextFactory();
+        using var context = dbcFactory.CreateDbContext();
+        
+        try
+        {
+            created = context.Database.EnsureCreated();
+        }
+        finally
+        {
+            context.Database.EnsureDeleted();
+        }
+
+        created.Should().BeTrue();
+    }
+    
+    [Test]
+    public void Correct_connection_string()
+    {
+        var dbcFactory = new DbContextFactory();
+        using var context = dbcFactory.CreateDbContext();
+
+        var connectionString = context.Database.GetConnectionString();
+
+        connectionString.Should().Contain(PathProvider.DataBaseFileName);
+    }
+}
