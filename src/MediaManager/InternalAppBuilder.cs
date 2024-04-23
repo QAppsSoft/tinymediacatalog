6552d8451a1529f39e9e559a822d40710fc52573
alex6dj <alexeiagueroalba@gmail.com>
2024-04-23 13:36:21 -0400

    Refactor AppBuilder and catch global exceptions

---
+++ src/MediaManager/InternalAppBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Avalonia;
+using ReactiveUI;
+using Serilog;
+
+namespace MediaManager;
+
+public class InternalAppBuilder(Func<AppBuilder> builder)
+{
+    private readonly Func<AppBuilder> _builder = builder ?? throw new ArgumentNullException(nameof(builder));
+
+    public void Run(string[] args)
+    {
+        RxApp.DefaultExceptionHandler = new ReactiveUiObservableExceptionHandler();
+        TaskScheduler.UnobservedTaskException += (_, eventArgs) =>
+        {
+            // here we can work with the exception, for example add it to our log file
+            Log.Fatal(eventArgs.Exception, "A global non caught exception happened");
+        };
+
+        try
+        {
+            // prepare and run your App here
+            _builder().StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception e)
+        {
+            // here we can work with the exception, for example add it to our log file
+            Log.Fatal(e, "A global non caught exception happened");
+        }
+        finally
+        {
+            // This block is optional. 
+            // Use the finally-block if you need to clean things up or similar
+            Log.CloseAndFlush();
+        }
+    }
+}
---
+++ src/MediaManager/ReactiveUIObservableExceptionHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reactive.Concurrency;
+using ReactiveUI;
+using Serilog;
+
+namespace MediaManager;
+
+public sealed class ReactiveUiObservableExceptionHandler : IObserver<Exception>
+{
+    public void OnNext(Exception value)
+    {
+        if (Debugger.IsAttached) Debugger.Break();
+
+        Log.Fatal(value, "A global non caught exception happened");
+
+        RxApp.MainThreadScheduler.Schedule(() => throw value) ;
+    }
+
+    public void OnError(Exception error)
+    {
+        if (Debugger.IsAttached) Debugger.Break();
+
+        Log.Fatal(error, "A global non caught exception happened");
+
+        RxApp.MainThreadScheduler.Schedule(() => throw error);
+    }
+
+    public void OnCompleted()
+    {
+        if (Debugger.IsAttached) Debugger.Break();
+        RxApp.MainThreadScheduler.Schedule(() => throw new NotImplementedException());
+    }
+}
