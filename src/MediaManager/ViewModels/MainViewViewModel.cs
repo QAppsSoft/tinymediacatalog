e5cd5c1a96614a5f4a3121820ec3e3a3db064d63
alex6dj <alexeiagueroalba@gmail.com>
2024-04-23 23:56:47 -0400

    Add MainViewViewModel ILogger

--- src/MediaManager/ViewModels/MainViewViewModel.cs
+++ src/MediaManager/ViewModels/MainViewViewModel.cs
@@ -1,17 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
 using MediaManager.ViewModels.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace MediaManager.ViewModels;
 
 public partial class MainViewViewModel : ViewModelBase
 {
+    private readonly ILogger _logger;
+
     [ObservableProperty]
     private IPageViewModel _current;
 
-    public MainViewViewModel(IEnumerable<IPageViewModel> pages)
+    public MainViewViewModel(IEnumerable<IPageViewModel> pages, ILogger<MainViewViewModel> logger)
     {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         Pages = pages.ToArray();
         Current = Pages[0];
     }
