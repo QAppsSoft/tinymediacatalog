63f188707e01537f1b53ab32074b3c38a1fa174a
alex6dj <alexeiagueroalba@gmail.com>
2024-04-23 23:52:17 -0400

    Cleanup LoggingConfiguration and add string Logging constant

--- src/MediaManager/DependencyInjection/LoggingConfiguration.cs
+++ src/MediaManager/DependencyInjection/LoggingConfiguration.cs
@@ -4,11 +4,9 @@ namespace MediaManager.DependencyInjection;
 
 public sealed class LoggingConfiguration
 {
+    public const string Logging = "Logging";
     public string LogFileName { get; init; } = null!;
-
     public long LimitBytes { get; init; }
-
     public LogEventLevel DefaultLogLevel { get; init; }
-
     public LogEventLevel MicrosoftLogLevel { get; init; }
 }
