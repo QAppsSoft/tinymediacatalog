07138a59c82dd730f919c07a2abc2a7f4de09710
alex6dj <alexeiagueroalba@gmail.com>
2024-04-25 14:12:07 -0400

    Add domain fixtures

---
+++ tests/TestsCommons/Domain/DatabaseSeedDataFixture.cs
@@ -0,0 +1,51 @@
+﻿using Domain;
+using Domain.Models;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestsCommons.Domain;
+
+public sealed class DatabaseSeedDataFixture : IDisposable
+{
+    private readonly StorageFixture _storageFixture = new();
+    public Func<MediaManagerDatabaseContext> MediaManagerDatabaseContextFactory { get; }
+    private readonly string _databasePath;
+
+    public DatabaseSeedDataFixture()
+    {
+        _databasePath = _storageFixture.GetTemporalFileName(".db");
+        
+        using var databaseContext = GetContext();
+
+        databaseContext.Database.EnsureCreated();
+
+        databaseContext.Persons.Add(new Person
+        {
+            Name = "Matt Schulze", Profile = "https://www.themoviedb.org/person/31841",
+            Thumb = "https://image.tmdb.org/t/p/h632/wFOVJr3dxEs5kC2rtS5iV3XbU0C.jpg", UniqueIds = new List<UniqueId>
+            {
+                new() { Name = UniqueId.ValidNames.Tmdb, Id = "31841" },
+            },
+        });
+
+        databaseContext.SaveChanges();
+
+        MediaManagerDatabaseContextFactory = GetContext;
+    }
+
+    private MediaManagerDatabaseContext GetContext()
+    {
+        var builder = new SqliteConnectionStringBuilder { DataSource =  _databasePath};
+
+        var options = new DbContextOptionsBuilder<MediaManagerDatabaseContext>();
+        options.UseSqlite(builder.ConnectionString);
+
+        return new MediaManagerDatabaseContext(options.Options);
+    }
+
+    public void Dispose()
+    {
+        _ = GetContext().Database.EnsureDeleted();
+        _storageFixture.Dispose();
+    }
+}
---
+++ tests/TestsCommons/Domain/DbContextFactoryFixture.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestsCommons.Domain;
+
+public sealed class DbContextFactoryFixture : IDbContextFactory<MediaManagerDatabaseContext>, IDisposable
+{
+    private readonly DatabaseSeedDataFixture _fixture = new();
+    
+    public MediaManagerDatabaseContext CreateDbContext()
+    {
+        return _fixture.MediaManagerDatabaseContextFactory();
+    }
+
+    public void Dispose()
+    {
+        _fixture.Dispose();
+    }
+}
