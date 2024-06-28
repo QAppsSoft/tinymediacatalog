f882117b4f013f09e1fc0ebd289713379a76baee
alex6dj <alexeiagueroalba@gmail.com>
2024-06-28 11:16:29 -0400

    Manually implement GetHashCode in GeneralSettings to support array comparison

--- src/Services/Services.Settings/Models/GeneralSettings.cs
+++ src/Services/Services.Settings/Models/GeneralSettings.cs
@@ -3,4 +3,21 @@
 public record GeneralSettings(string[] MovieSources, string[] TvShowSources)
 {
     public static GeneralSettings Default { get; } = new([], []);
+
+    public override int GetHashCode()
+    {
+        var hashCode = 0;
+
+        foreach (var movieSource in MovieSources)
+        {
+            hashCode ^= StringComparer.Ordinal.GetHashCode(movieSource);
+        }
+
+        foreach (var tvShowSource in TvShowSources)
+        {
+            hashCode ^= StringComparer.Ordinal.GetHashCode(tvShowSource);
+        }
+
+        return hashCode;
+    }
 }
