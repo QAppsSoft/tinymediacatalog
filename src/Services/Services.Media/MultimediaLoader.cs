54a044d938fdd9b1c2d4faa2c9228bc50c491393
alex6dj <alexeiagueroalba@gmail.com>
2024-08-12 11:54:30 -0400

    Initial files classification and loading implementation

--- src/Services/Services.Media/MultimediaLoader.cs
+++ src/Services/Services.Media/MultimediaLoader.cs
@@ -1,5 +1,9 @@
-﻿using Domain;
+﻿using System.Collections.Immutable;
+using System.Reactive.Linq;
+using Common.Extensions.Enumerable;
+using Domain;
 using Domain.Models.Movie;
+using Domain.Models.Multimedia;
 using Microsoft.EntityFrameworkCore;
 using Services.Abstractions.Media;
 using Services.Abstractions.Settings;
@@ -37,17 +41,74 @@ public class MultimediaLoader(
 
             await UpdateFilesAsync(movie, path).ConfigureAwait(false);
             
-            //await nfoReader.LoadMovieAsync(movie.Id).ConfigureAwait(false);
+            await nfoReader.LoadMovieAsync(movie.Id).ConfigureAwait(false);
             
             return movie.Id;
         }
     }
 
-    private Task UpdateFilesAsync(MovieContainer movie, string path)
+    private async Task UpdateFilesAsync(MovieContainer movie, string path)
     {
+        var generalSettings = await setting.Value;
         
-        
-        throw new NotImplementedException();
+        var files = Directory.EnumerateFiles(path).ToImmutableArray();
+
+        var videos = files.Where(file => IsVideo(file, generalSettings.SupportedVideoTypes)).ToImmutableArray();
+        var audios = files.Where(file => IsAudio(file, generalSettings.SupportedAudioTypes)).ToImmutableArray();
+        var subtitles = files.Where(file => IsSubtitle(file, generalSettings.SupportedSubtitleTypes)).ToImmutableArray();
+        var pics = files.Where(IsImage).ToImmutableArray();
+        var nfo = files.Where(IsNfo).ToImmutableArray();
+        var others = files.RemoveRange(videos, StringComparer.Ordinal)
+            .RemoveRange(audios, StringComparer.Ordinal)
+            .RemoveRange(subtitles, StringComparer.Ordinal)
+            .RemoveRange(pics, StringComparer.Ordinal)
+            .RemoveRange(nfo, StringComparer.Ordinal);
+
+        movie.Files
+            .AddRange(videos.Select(file => new VideoFile { FilePath = file })) // TODO: Get Video info and others
+            .AddRange(audios.Select(file => new AudioFile { FilePath = file }))
+            .AddRange(subtitles.Select(file => new SubtitleFile { FilePath = file }))
+            .AddRange(pics.Select(file => new ImageFile { FilePath = file }))
+            .AddRange(nfo.Select(file => new NfoFile { FilePath = file }))
+            .AddRange(others.Select(file => new OtherFile { FilePath = file }));
+    }
+
+    private static bool IsNfo(string file)
+    {
+        var fileExtension = Path.GetExtension(file);
+        return string.Equals(fileExtension, ".nfo", StringComparison.Ordinal);
+    }
+
+    private static bool IsImage(string file)
+    {
+        var fileExtension = Path.GetExtension(file);
+        return string.Equals(fileExtension, ".jpg", StringComparison.Ordinal);
+    }
+
+    private static bool IsSubtitle(string file, IEnumerable<SubtitleType> subtitleTypes)
+    {
+        var fileExtension = Path.GetExtension(file);
+        var videoExtensions = subtitleTypes.Select(x => x.Extension);
+        return ContainsExtension(fileExtension, videoExtensions);
+    }
+
+    private static bool IsAudio(string file, IEnumerable<AudioType> audioTypes)
+    {
+        var fileExtension = Path.GetExtension(file);
+        var videoExtensions = audioTypes.Select(x => x.Extension);
+        return ContainsExtension(fileExtension, videoExtensions);
+    }
+
+    private static bool IsVideo(string file, IEnumerable<VideoType> videoTypes)
+    {
+        var fileExtension = Path.GetExtension(file);
+        var videoExtensions = videoTypes.Select(x => x.Extension);
+        return ContainsExtension(fileExtension, videoExtensions);
+    }
+
+    private static bool ContainsExtension(string fileExtension, IEnumerable<string> baseExtensions)
+    {
+        return baseExtensions.Any(x => string.Equals(x, fileExtension, StringComparison.Ordinal));
     }
 
     public Task<Guid> UpdateMovieAsync(string path)
