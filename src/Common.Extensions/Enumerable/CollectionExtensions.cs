e4bc3707fc403425c92b4f92939283d5edf93029
alex6dj <alexeiagueroalba@gmail.com>
2024-08-12 11:43:55 -0400

    ICollection AddRange extension

---
+++ src/Common.Extensions/Enumerable/CollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Common.Extensions.Enumerable;
+
+public static class CollectionExtensions
+{
+    public static ICollection<T> AddRange<T>(this ICollection<T> collection, IEnumerable<T> range)
+    {
+        foreach (var item in range)
+        {
+            collection.Add(item);
+        }
+
+        return collection;
+    }
+}
