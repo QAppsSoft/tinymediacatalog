740577e4002fa98433fcc7aa69f01c7c65dd95ac
alex6dj <alexeiagueroalba@gmail.com>
2024-08-14 13:41:40 -0400

    Implement missing media processors

---
+++ src/Services/Services.Media/Strategy/Processors/AudioFileProcessor.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Models.Multimedia;
+using FFMpegCore;
+
+namespace Services.Media.Strategy.Processors;
+
+public class AudioFileProcessor : NoMediaFileBase<AudioFile>
+{
+    public override FileKind MediaKind { get; } = FileKind.Audio;
+    
+    public override async Task<MultimediaFile> PrepareAsync(string path)
+    {
+        var mediaInfo = await FFProbe.AnalyseAsync(path).ConfigureAwait(false);
+
+        var file = GetBasicData(path);
+
+        if (mediaInfo.PrimaryAudioStream != null)
+        {
+            var audio = mediaInfo.PrimaryAudioStream;
+
+            file.BitRate = (int)audio.BitRate; // TODO: Make bitrate long maybe
+            file.Codec = audio.CodecName;
+            file.Duration = audio.Duration;
+            file.Language = audio.Language ?? string.Empty;
+            file.BitRate = (int)audio.BitRate;
+        }
+
+        return file;
+    }
+}
---
+++ src/Services/Services.Media/Strategy/Processors/VideoFileProcessor.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+using Common.Extensions.Enumerable;
+using Domain.Models.Multimedia;
+using FFMpegCore;
+using AudioStream = Domain.Models.Multimedia.AudioStream;
+using SubtitleStream = Domain.Models.Multimedia.SubtitleStream;
+
+namespace Services.Media.Strategy.Processors;
+
+public class VideoFileProcessor : NoMediaFileBase<VideoFile>
+{
+    public override FileKind MediaKind { get; } = FileKind.Video;
+    
+    public override async Task<MultimediaFile> PrepareAsync(string path)
+    {
+        var mediaInfo = await FFProbe.AnalyseAsync(path).ConfigureAwait(false);
+
+        var file = GetBasicData(path);
+
+        if (mediaInfo.PrimaryVideoStream != null)
+        {
+            var video = mediaInfo.PrimaryVideoStream;
+
+            file.BitRate = (int)video.BitRate; // TODO: Make bitrate long maybe
+            file.Codec = video.CodecName;
+            file.Duration = video.Duration;
+            file.Resolution = new Resolution(video.Width, video.Height);
+            file.FrameRate = video.FrameRate.ToString(CultureInfo.InvariantCulture);
+        }
+        
+        // TODO: Get additional audio and subtitle data
+            
+        var audios = mediaInfo.AudioStreams.Select(audio => new AudioStream
+        {
+            BitRate = (int)audio.BitRate,
+            Channels = audio.Channels.ToString(CultureInfo.InvariantCulture),
+            Language = audio.Language ?? string.Empty,
+        });
+
+        file.Audios.AddRange(audios);
+
+        var subtitles = mediaInfo.SubtitleStreams.Select(subtitle => new SubtitleStream()
+        {
+            Format = subtitle.CodecName,
+            Language = subtitle.Language ?? string.Empty,
+        });
+
+        file.Subtitles.AddRange(subtitles);
+
+        return file;
+    }
+}
